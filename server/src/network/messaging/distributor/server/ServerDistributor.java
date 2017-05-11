18799bfe55d307f41aed111f856259f6cb02b426
Jose Martins <lluismmartins7@gmail.com>
2017-05-11 01:24:11 +0100

    ola david

--- server/src/network/messaging/distributor/server/ServerDistributor.java
+++ server/src/network/messaging/distributor/server/ServerDistributor.java
@@ -6,6 +6,10 @@ import network.Utils;
 import network.messaging.Message;
 import network.messaging.distributor.Distributor;
 import org.json.JSONObject;
+import org.postgresql.geometric.PGpoint;
+
+import java.sql.Timestamp;
+
 
 /**
  * Created by joao on 5/10/17.
@@ -25,13 +29,13 @@ public class ServerDistributor extends Distributor {
     public ServerDistributor(Server server){
         this.server = server;
         addAction(SET_MODE, (Message m) -> setMode(m));
-        addAction(ADD_USER, (Message m) -> setMode(m));
-        addAction(ADD_CHAT_GROUP, (Message m) -> setMode(m));
-        addAction(GET_CHAT_GROUP, (Message m) -> setMode(m));
+        addAction(ADD_USER, (Message m) -> addUser(m));
+        addAction(ADD_CHAT_GROUP, (Message m) -> addChatGroup(m));
+        addAction(GET_CHAT_GROUP, (Message m) -> getChatGroup(m));
 
     }
 
-    
+
     public void setMode(Message m){
         server.setMode((int)m.getContent());
     }
@@ -42,13 +46,23 @@ public class ServerDistributor extends Distributor {
         Utils.db.add_user(obj.getString("token"));
     }
 
-    public void setAddChatGroup(Message m){
+    public void addChatGroup(Message m){
+        JSONObject obj = new JSONObject((String)m.getContent());
 
+
+        PGpoint point = new PGpoint(obj.getDouble("lat"),obj.getDouble("long"));
+        Timestamp ts = new Timestamp(obj.getLong("timestamp"));
+        String creator = obj.getString("lat");
+
+
+        Utils.db.add_chatroom(point,ts,creator);
 
     }
 
     public void getChatGroup(Message m){
+        JSONObject obj = new JSONObject((String)m.getContent());
 
+        Utils.db.add_user(obj.getString("token"));
 
     }
 
