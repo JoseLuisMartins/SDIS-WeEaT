638f9eee69bd8ff9c8964ed5643afc15fdb0ac27
mferreira96 <pount@sapo.pt>
2017-05-17 21:47:32 +0100

    added possibility to change type of  map and added a customized Alertdialog

--- Client/app/src/main/java/com/example/josemartins/sdis_weeat/gui/ChooseLocal.java
+++ Client/app/src/main/java/com/example/josemartins/sdis_weeat/gui/ChooseLocal.java
@@ -1,40 +1,37 @@
 package com.example.josemartins.sdis_weeat.gui;
 
 import android.app.FragmentTransaction;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Button;
 
 import com.example.josemartins.sdis_weeat.R;
+import com.google.android.gms.maps.GoogleMap;
 
 public class ChooseLocal extends AppCompatActivity {
 
-    private Spinner time;
-    private TextView choseLocalTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_local);
 
-
-
-        time = (Spinner) findViewById(R.id.time);
-
-        //Utils.client.makeRequest("AuthUser","POST","Body-> handle location".getBytes());
-
         android.app.FragmentManager fm = getFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         MapFragment mf =new MapFragment();
 
 
+        Button btTerrain = (Button) findViewById(R.id.terrain);
+        Button btHybrid = (Button) findViewById(R.id.hybrid);
+        Button btSatellite = (Button) findViewById(R.id.satellite);
+        Button btNormal = (Button) findViewById(R.id.normal);
+
+        btTerrain.setOnClickListener(v -> mf.changeMapType(GoogleMap.MAP_TYPE_TERRAIN));
+        btHybrid.setOnClickListener(v -> mf.changeMapType(GoogleMap.MAP_TYPE_HYBRID));
+        btSatellite.setOnClickListener(v -> mf.changeMapType(GoogleMap.MAP_TYPE_SATELLITE));
+        btNormal.setOnClickListener(v -> mf.changeMapType(GoogleMap.MAP_TYPE_NORMAL));
+
         ft.add(R.id.map,mf);
         ft.commit();
-
     }
-
 }
